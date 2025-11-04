2fd9e8f29175f2eb1d5c54e28714298eff92735b
E11aW <Ellaraindeer2022@gmail.com>
2025-11-03 17:13:01 -0800

    Removed flip cards

--- src/pages/homepage/home.js
+++ src/pages/homepage/home.js
@@ -2,12 +2,9 @@ import './home.scss';
 import { MaxWidth } from '../../components/MaxWidth/MaxWidth';
 import { TextImage } from '../../components/TextImage/TextImage';
 import { ImageOverlay } from '../../components/ImageOverlay/ImageOverlay';
-import { CardGrid } from '../../components/CardGrid/CardGrid';
 import { SealRoller } from '../../components/SealRoller/SealRoller';
 import Placeholder from '../../assets/Home/placeholder.png';
-import GraphPlaceholder from '../../assets/Home/graph_placeholder.png'
 import Glacier from '../../assets/Home/glacier.jpg'
-import { Seal } from '../../components/Seal/Seal';
 
 export const Homepage = () => {
   return (
@@ -19,22 +16,8 @@ export const Homepage = () => {
       />
       <MaxWidth>
         <h2 className='subheader-need-padding'>Game</h2>
-        {/* <div className='game-section'>
-          <Button className='game-button'
-            buttonText='Play Game'
-            buttonLink='/game'
-          />
-        </div> */}
         <SealRoller />
-        <CardGrid
-          subheader='Data'
-          members={[
-            { image: GraphPlaceholder, name: 'Graph1', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis sed risus in ultricies. Mauris vitae malesuada tellus. Ut sed urna dignissim diam viverra efficitur non vel lacus. Aliquam accumsan quis nulla eu fringilla. Vivamus tempus ante non ligula pulvinar, id gravida purus ultrices. Etiam sed auctor neque, et tristique augue. Cras eget lorem diam. In porta bibendum neque, non.' },
-            { image: GraphPlaceholder, name: 'Graph2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis sed risus in ultricies. Mauris vitae malesuada tellus. Ut sed urna dignissim diam viverra efficitur non vel lacus. Aliquam accumsan quis nulla eu fringilla. Vivamus tempus ante non ligula pulvinar, id gravida purus ultrices. Etiam sed auctor neque, et tristique augue. Cras eget lorem diam. In porta bibendum neque, non.' },
-            { image: GraphPlaceholder, name: 'Graph3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis sed risus in ultricies. Mauris vitae malesuada tellus. Ut sed urna dignissim diam viverra efficitur non vel lacus. Aliquam accumsan quis nulla eu fringilla. Vivamus tempus ante non ligula pulvinar, id gravida purus ultrices. Etiam sed auctor neque, et tristique augue. Cras eget lorem diam. In porta bibendum neque, non.' },
-          ]}
-
-        />
+        <h2 className='subheader-need-padding'>Data</h2>
         <h2 className='about-us-subheader'>About Us</h2>
         <TextImage
           text='We are a small team of 4 developers working to spread awareness about climate change and its impacts on arctic habitats. We all attend the University of Washington Bothell and completed this website as our final project for a Web Developing class. Our goal was to improve our frontend skills, connecting that with backend data to create a website that is visually interesting while also presenting the important data.'
